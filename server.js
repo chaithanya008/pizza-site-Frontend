fec73e1987b82d7411db0afa7b523f5a3e041125
chaithanya008 <krishna.chaithanya.8@gmail.com>
2020-05-23 21:03:39 +0530

    Server.js issue resolved

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/pizza-site-frontend'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/pizza-site-frontend/index.html'));
+app.get('/*', function(req,res) {    
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
