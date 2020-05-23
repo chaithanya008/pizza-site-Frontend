51e6bf388f6d1943ef8c6525efa1a4fed8f76ddc
chaithanya008 <krishna.chaithanya.8@gmail.com>
2020-05-23 21:11:55 +0530

    Server root path changed

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/pizza-site-Frontend'));
 
 app.get('/*', function(req,res) {    
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/pizza-site-Frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
