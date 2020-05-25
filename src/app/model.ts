36d59a67b35d741e2b5b65c8ab137dd6db8996b1
chaithanya008 <krishna.chaithanya.8@gmail.com>
2020-05-25 23:38:29 +0530

    On track order page display order info

--- src/app/model.ts
+++ src/app/model.ts
@@ -17,3 +17,25 @@ export interface Item {
     price_usd: number;
     type_id: number;
 }
+
+export interface OrderDetail {
+    id: number;
+    name: string;
+    price_eur: number;
+    price_usd: number;
+    address: string;
+    contact: string;
+    status: string;
+    items: OrderItem[];
+}
+
+export interface OrderItem {
+    id: number;
+    item_id: number;
+    order_id: number;
+    price_eur: number;
+    price_usd: number;
+    quantity: number;
+    total_price_eur: number;
+    total_price_usd: number;
+}
--- src/app/track-order/track-order.component.ts
+++ src/app/track-order/track-order.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpService } from '../http.service';
+import { CartService } from '../cart.service';
+import { Item, OrderDetail } from '../model';
 
 @Component({
   selector: 'app-track-order',
@@ -9,13 +11,17 @@ import { HttpService } from '../http.service';
 })
 export class TrackOrderComponent implements OnInit {
 
-  orderDetail: any;
+  orderDetail: OrderDetail;
   orderForm: FormGroup;
   alertType: boolean;
   alertMessage: string;
 
+  menus: Item[];
+  orderItems: any[] = [];
+
   constructor(
     private httpService: HttpService,
+    private cartService: CartService,
     private fb: FormBuilder
   ) {
     this.orderForm = this.fb.group({
@@ -35,10 +41,38 @@ export class TrackOrderComponent implements OnInit {
       (res: any) => {
         this.orderDetail = res.data;
         this.alertType = res.data != null;
+        this.prepareOrderItems();
       },
       err => {
         this.alertType = false;
       }
     );
+  }
+
+  // Extracting order
+  prepareOrderItems() {
+    this.menus = this.cartService.getMenu() || []; // Fetch Menu
+
+    this.menus = this.menus.map((m: any) => m.items)
+        .reduce((previous, current) => previous.concat(current)); // Merging all items in single array
+
+    // Itrating one by one
+    this.orderDetail.items.forEach(item => {
+      const existedMenu = this.menus.filter(i => i.id == item.id); // Check if item matched with menu items
+      const menu = existedMenu.length > 0 ? existedMenu[0] : null; // If matched then pick first value
+
+      // If menu matched
+      if (menu != null) {
+        // Push into cart items to display on checkout page
+        this.orderItems.push({
+          id: menu.id,
+          name: menu.name,
+          description: menu.description,
+          qty: item.quantity,
+          priceEur: item.total_price_eur,
+          priceUsd: item.total_price_usd
+        });
+      }
+    });
   }
 }
